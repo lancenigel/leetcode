014feb0df8c3e94b988a0797b7a7516bf3e8bb72
lancenigel <lancenigelmadarang@gmail.com>
2024-01-28 23:54:27 -0500

    Jan 28

--- Linked List/merge_sorted.py
+++ Linked List/merge_sorted.py
@@ -14,3 +14,17 @@ class Solution:
         node.next = list1 or list2
 
         return dummy.next
+
+
+# Recursive
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
+        lil.next = self.mergeTwoLists(lil.next, big)
+        return lil
