bc5dbcd53749a7d80b405f0d07e0a1ce62151d42
lancenigel <lancenigelmadarang@gmail.com>
2024-02-18 09:38:50 -0500

    feb 19

--- Trees/diameter_of_binary_tree.py
+++ Trees/diameter_of_binary_tree.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res = 0
+
+        res = [0]
 
         def dfs(root):
-            nonlocal res
-
             if not root:
-                return 0
+                return -1  # heigh of null tree
             left = dfs(root.left)
             right = dfs(root.right)
-            res = max(res, left + right)
+
+            res[0] = max(res[0], 2 + left + right)
 
             return 1 + max(left, right)
 
         dfs(root)
-        return res
+        return res[0]
