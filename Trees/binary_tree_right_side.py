2de7f8a394961c08812bce0657c6e572e86b5461
lancenigel <lancenigelmadarang@gmail.com>
2024-02-27 18:53:09 -0500

    feb 27

--- Trees/binary_tree_right_side.py
+++ Trees/binary_tree_right_side.py
@@ -8,17 +8,18 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
         res = []
-
         q = collections.deque([root])
 
         while q:
-            rightSide = None
+
             qLen = len(q)
+            rightSide = None
 
             for i in range(qLen):
                 node = q.popleft()
 
                 if node:
+                    # at the end of the for loop, it will take the right most side node
                     rightSide = node
                     q.append(node.left)
                     q.append(node.right)
